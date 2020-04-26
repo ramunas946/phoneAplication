4068ced53edf76f0436f7c5f16ae03d0579300be
ramunas946 <55541090+ramunas946@users.noreply.github.com>
2020-04-26 13:43:15 +0300

    added checkbox funtioning saves on click //needs checkbox active if true

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,24 +18,56 @@ import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView password_text_input,username_text_input;
     EditText username, password;
     Button Registerbtn, Loginbtn;
+    CheckBox remembarbtn;
+    public static final String MyPREFERENCES = "MyPrefs" ;
+    public static final String Name = "username";
+    public static final String pass = "password";
+    public static final String remeb = "checked";
+    private String Username;
+    private String Password;
+    private Boolean checked;
+
+    SharedPreferences sharedpreferences;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       final EditText username = findViewById(R.id.username);
-       final EditText password = findViewById(R.id.password);
+        final TextView textViewuser = findViewById(R.id.password_text_input);
+        final TextView textViewpass = findViewById(R.id.username_text_input);
+        final EditText username = findViewById(R.id.username);
+        final EditText password = findViewById(R.id.password);
+        final CheckBox remembarbtn = findViewById(R.id.remembarbtn);
 
-        Button register = findViewById(R.id.Registerbtn);
-        Button submit = findViewById(R.id.Loginbtn);
+
+
+        final Button register = findViewById(R.id.Registerbtn);
+        final Button submit = findViewById(R.id.Loginbtn);
+        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        String usernames = sharedpreferences.getString(Name, null);
+        String passwords = sharedpreferences.getString(pass, null);
+        Boolean checked = sharedpreferences.getBoolean(remeb, false);
+
+        remembarbtn.setActivated(sharedpreferences.getBoolean("checked", false));
+        if(checked = true) {
+            username.setText(sharedpreferences.getString("username", null));
+            password.setText(sharedpreferences.getString("password", null));
+        }
+        else{
+
+        }
+
+
+
 
         submit.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View focusView) {
+            @Override
+            public void onClick(View view) {
                 boolean cancel = false;
-                focusView = null;
                 if (!username_crediantials(username.getText().toString())) {
                     username.setError(getString(R.string.login_faill_username));
                     cancel = true;
@@ -44,21 +80,37 @@ public class MainActivity extends AppCompatActivity {
                 if (cancel) {
 
                 } else {
-                    Toast.makeText(MainActivity.this, username.getText().toString() + "\n" + password.getText().toString(),
-                            Toast.LENGTH_SHORT).show();
                     startActivity(new Intent(MainActivity.this, Login.class));
                 }
-
-
             }
         });
         register.setOnClickListener(new View.OnClickListener() {
             public void onClick(View focusView) {
-                    startActivity(new Intent(MainActivity.this, Register.class));
-                }
+                startActivity(new Intent(MainActivity.this, Register.class));
+            }
         });
-
+        remembarbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+                if (remembarbtn.isChecked()){
+                    remembarbtn.setChecked(true);
+                    editor.putString(Name, username.getText().toString());
+                    editor.putString(pass, password.getText().toString());
+                    editor.putBoolean("checked", true);
+                    editor.apply();
+                }
+                else{
+                    remembarbtn.setChecked(false);
+                    Toast.makeText(MainActivity.this, "ur login wont be saved",
+                            Toast.LENGTH_SHORT).show();
+                    editor.clear();
+                    editor.apply();
+                }
+            }
+        });
     }
+
 
     private boolean username_crediantials(String crediantails) {
         final String CREDENTIALS_PATERN = "^([a-zA-Z0-9]{3,20})+$";
