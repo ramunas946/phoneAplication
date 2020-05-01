59e061c987f40f48e8cbc038afe1b6189ce56af5
ramunas946 <55541090+ramunas946@users.noreply.github.com>
2020-05-01 13:11:22 +0300

    added buttons to new activity

--- app/src/main/java/com/example/myapplication/Login.java
+++ app/src/main/java/com/example/myapplication/Login.java
@@ -1,9 +1,7 @@
 package com.example.myapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,28 +11,13 @@ public class Login extends AppCompatActivity implements View.OnClickListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button button =findViewById(R.id.Registerbtn);
-        Button button1 =findViewById(R.id.Loginbtn);
-        Button button2 =findViewById(R.id.Mainbutton);
-
-        button.setOnClickListener(this);
-        button1.setOnClickListener(this);
-        button2.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.Registerbtn:
-                startActivity(new Intent(Login.this, Register.class));
-                break;
-            case R.id.Loginbtn:
 
-                break;
-            case R.id.Mainbutton:
-                startActivity(new Intent(Login.this, MainActivity.class));
-                break;
         }
     }
 
